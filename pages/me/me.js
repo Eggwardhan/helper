d63a126053871e3323f293c02dc7eedcedfdef74
eggward <hshzjtkr@126.com>
2019-02-27 21:03:19 +0800

    renew the me.wxml

--- pages/me/me.js
+++ pages/me/me.js
@@ -24,7 +24,6 @@ Page({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-        
           wx.getUserInfo({
             success: res => {
               app.globalData.userInfo=res.userInfo;
